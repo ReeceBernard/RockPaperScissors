ae0cd3d2272fa11a9329cae5d5b1ccd5666cf060
Reece Bernard <reecebernard@Reeces-MBP.attlocal.net>
2022-02-22 18:19:04 -0500

    refactor

--- rps/routes.py
+++ rps/routes.py
@@ -6,125 +6,62 @@ import numpy as np
 
 last_play = None
 last_result = None
-game_state = [0,0,0]
+game_state = [0, 0, 0]
 weights = {
-        "W": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
-        },
-        "L": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
-        },
-        "T": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
-        }
-    }
+    "W": {
+        "R": {"R": 1, "P": 1, "S": 1},
+        "P": {"R": 1, "P": 1, "S": 1},
+        "S": {"R": 1, "P": 1, "S": 1},
+    },
+    "L": {
+        "R": {"R": 1, "P": 1, "S": 1},
+        "P": {"R": 1, "P": 1, "S": 1},
+        "S": {"R": 1, "P": 1, "S": 1},
+    },
+    "T": {
+        "R": {"R": 1, "P": 1, "S": 1},
+        "P": {"R": 1, "P": 1, "S": 1},
+        "S": {"R": 1, "P": 1, "S": 1},
+    },
+}
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     global game_state
-    game_state = [0,0,0]
+    game_state = [0, 0, 0]
     global weights
     weights = {
         "W": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
+            "R": {"R": 1, "P": 1, "S": 1},
+            "P": {"R": 1, "P": 1, "S": 1},
+            "S": {"R": 1, "P": 1, "S": 1},
         },
         "L": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
+            "R": {"R": 1, "P": 1, "S": 1},
+            "P": {"R": 1, "P": 1, "S": 1},
+            "S": {"R": 1, "P": 1, "S": 1},
         },
         "T": {
-            "R": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "P": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            },
-            "S": {
-                "R": 1,
-                "P": 1,
-                "S": 1
-            }
-        }
+            "R": {"R": 1, "P": 1, "S": 1},
+            "P": {"R": 1, "P": 1, "S": 1},
+            "S": {"R": 1, "P": 1, "S": 1},
+        },
     }
-    return render_template('home.html', game_state=game_state, weights=weights, robot_choice="", player_choice="")
+    return render_template(
+        "home.html",
+        game_state=game_state,
+        weights=weights,
+        robot_choice="",
+        player_choice="",
+    )
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
+
 
 @app.route("/play_rock")
 def play_rock():
@@ -142,8 +79,15 @@ def play_rock():
         game_state[2] += 1
         update_weights(weights, 0.05, robot_choice, last_play, last_result)
         last_result = "T"
-    last_play = 'R'
-    return render_template('home.html', game_state=game_state, weights=weights, robot_choice=robot_choice, player_choice="R")
+    last_play = "R"
+    return render_template(
+        "home.html",
+        game_state=game_state,
+        weights=weights,
+        robot_choice=robot_choice,
+        player_choice="R",
+    )
+
 
 @app.route("/play_paper")
 def play_paper():
@@ -161,8 +105,15 @@ def play_paper():
         game_state[2] += 1
         update_weights(weights, 0.05, robot_choice, last_play, last_result)
         last_result = "T"
-    last_play = 'P'
-    return render_template('home.html', game_state=game_state, weights=weights, robot_choice=robot_choice, player_choice="P")
+    last_play = "P"
+    return render_template(
+        "home.html",
+        game_state=game_state,
+        weights=weights,
+        robot_choice=robot_choice,
+        player_choice="P",
+    )
+
 
 @app.route("/play_scissors")
 def play_scissors():
@@ -180,9 +131,16 @@ def play_scissors():
         game_state[2] += 1
         update_weights(weights, 0.05, robot_choice, last_play, last_result)
         last_result = "T"
-    last_play = 'S'
+    last_play = "S"
     print(weights)
-    return render_template('home.html', game_state=game_state, weights=weights, robot_choice=robot_choice, player_choice="S")
+    return render_template(
+        "home.html",
+        game_state=game_state,
+        weights=weights,
+        robot_choice=robot_choice,
+        player_choice="S",
+    )
+
 
 @app.route("/restart")
 def restart():
